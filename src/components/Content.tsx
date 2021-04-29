d873d18a7dc480d29b775d142608ca7790ac2170
vitormedeiros911 <vitormedeiros911@outlook.com>
2021-04-29 18:18:35 -0300

    Desafio finalizado

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,26 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from "./MovieCard";
+import { MovieProps } from "../App";
+
+interface ContentProps {
+  movies: Array<MovieProps>;
+}
+
+export function Content(props: ContentProps) {
+  return (
+    <>
+      <main>
+        <div className="movies-list">
+          {props.movies.map((movie) => (
+            <MovieCard
+              key={movie.imdbID}
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </>
+  );
 }
